18d82235b4b329b5920b9164c00dbeac81428f4e
matgdev <math.minp@gmail.com>
2025-04-04 08:29:33 -0400

    add mock data

---
+++ src/dataAccess.jsx
@@ -0,0 +1,17 @@
+import data from "./mock_data.json"
+
+export function getContentById(id){
+    const view = data.filter(d => d.id === id);
+    if (view.length === 1) return view[0];
+    return null;
+}
+
+export function getContentList(limit = 9, offset = 0){
+    const start = offset * limit;
+    const view = data.slice(start, start + limit).map((item) => {
+        const {id, title, thumbnail, content, date} = item;
+        const short_content = content.slice(0, 60);
+        return {id, title, thumbnail, content: short_content, date}
+    });
+    return view;
+}
