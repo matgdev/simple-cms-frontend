68054fabf84ac1541ff701c81989f7dd71c50dc1
matgdev <math.minp@gmail.com>
2025-04-15 09:09:20 -0300

    update: adjust pagination bar for narrow screens

--- src/PaginationBar.jsx
+++ src/PaginationBar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Pagination } from "react-bootstrap";
+import { Container, Pagination, Row, Stack } from "react-bootstrap";
 
 const halfNumberOfPages = 3; // pages shown = 2 * halfNumberOfPages + 1 + [2*Ellipses + firstPage + lastPage]
 
@@ -30,14 +30,16 @@ export function PaginationBar({onChange, lastPage, currentPage}){
     }
 
     return(
-        <Pagination className="justify-content-center m-0">
-            <Pagination.First onClick={() => handleClick(1)} />
-            <Pagination.Prev onClick={() => handleClick(Math.max(currentPage - 1, 1))}/>
-
-            {pages}
-
-            <Pagination.Next onClick={() => handleClick(Math.min(currentPage + 1, lastPage))}/>
-            <Pagination.Last onClick={() => handleClick(lastPage)}/>
-        </Pagination>
+        <Container>
+            <Pagination className="justify-content-center mx-auto my-0 w-100 p-0">
+                <Stack direction="horizontal" className="flex-wrap w-100 justify-content-center m-0 p-0">
+                    <Pagination.First onClick={() => handleClick(1)} />
+                    <Pagination.Prev onClick={() => handleClick(Math.max(currentPage - 1, 1))}/>
+                    {pages}
+                    <Pagination.Next onClick={() => handleClick(Math.min(currentPage + 1, lastPage))}/>
+                    <Pagination.Last onClick={() => handleClick(lastPage)}/>
+                </Stack>
+            </Pagination>
+        </Container>
     );
 }
